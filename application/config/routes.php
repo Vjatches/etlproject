6ff86d5b0869984fdc7df2089e55a97487ed8a8f
Unknown <Babanin_abv@mail.ru>
2018-10-15 11:18:58 +0200

    Installed composer and Symfony packages
    Composer and Symfony dom-crawler, css-selector packages

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,5 +53,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['news/(:any)']='news/view/$1';
 $route['news']='news';
 $route['(:any)']='pages/view/$1';*/
-$route['default_controller'] = 'mycontroller/home';
-$route['(:any)']='mycontroller/$1';
+$route['default_controller'] = 'etl/home';
+$route['(:any)']='etl/$1';
